9fe20b1bcf468defa36f73e8a8de9e3ff3958d2d
Andrew Kim <wk@ualberta.ca>
2019-05-26 18:06:52 +0000

    exercise joining arrays together

---
+++ concatenate-arrays.js
@@ -0,0 +1,18 @@
+function concat (array1, array2){
+  let concatArray = [];
+
+  for (let i = 0; i < array1.length; i++){
+    concatArray.push(array1[i]);
+  }
+
+  for (let i = 0; i < array2.length; i++){
+    concatArray.push(array2[i]);
+  }
+
+  return concatArray;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
